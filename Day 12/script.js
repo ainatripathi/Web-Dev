e7b10013d7d96cb1a62f0e9b7a81fcd1d0a56b0d
Aina Tripathi <ainatripathi07@gmail.com>
2024-11-30 12:30:40 +0530

    Add files via upload

---
+++ Day 12/script.js
@@ -0,0 +1,85 @@
+let num=20;
+console.log(num);
+console.log(typeof num);
+
+let num2=20;
+console.log("number is ",num2);
+console.log(typeof num2);
+
+let num3=20.5;
+console.log(num3);
+console.log(typeof num3);
+
+let cname='aina';
+let cname1="aina";
+let cname2='aina';
+console.log(cname, typeof cname);
+console.log(cname1, typeof cname1);
+console.log(cname2, typeof cname2);
+
+let stat=true;
+console.log(stat, typeof stat);
+let c=stat+5;
+console.log(c, typeof c);
+
+let num1;
+console.log(num1, typeof num1);
+
+let value1=900719925124740998n;
+
+let result1=value1+1n;
+console.log(result1);
+
+let value2=900719925124740998n;
+
+let number=null;
+console.log(number);
+
+let val1=Symbol("programiz");
+let val2=Symbol("programiz");
+
+console.log(val1==val2);
+
+let student ={
+    firstName: "John",
+    lastName: null,
+    class: 10
+}
+let number1=100;
+console.log(`value of num is ${num}`);
+
+let a=10;
+let b=20;
+let res=a+b;
+console.log("Result",res);
+res+=100;
+console.log("value of c ",res);
+
+let a1=10;
+console.log(a1);
+console.log(++a1);
+console.log(a1++);
+console.log(a1);
+console.log(--a1);
+console.log(a1--);
+console.log(a1);
+
+let a2=10;
+let b1=15;
+console.log(a2>b1);
+console.log(a2>=b1);
+console.log(a2<b1);
+console.log(a2<=b1);
+console.log(a2==b1);
+console.log(a2!=b1);
+
+let a3=10, t;
+t=(a3%2==0)?"Even no.":"Odd no.";
+console.log(t);
+
+let n=5, m=7;
+console.log((n<m)&& (m>n));
+console.log((n>m)|| (m<n));
+console.log((n<m)&& (m>n));
+
+
