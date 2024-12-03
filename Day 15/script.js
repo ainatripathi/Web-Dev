cab824b1e6a96fdaa8e855f91f64be009deb4cb1
Aina Tripathi <ainatripathi07@gmail.com>
2024-12-03 11:56:58 +0530

    Add files via upload

---
+++ Day 15/script.js
@@ -0,0 +1,15 @@
+let fruits=["mango", "apple", "cherry"];
+console.log(fruits);
+console.log(fruits[1]);
+let numbers = new Array(10, 20, 30);
+console.log(numbers);
+let emptyArray=[];
+console.log(emptyArray);
+let arr=[1,2,3];
+arr.push(4);
+console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
