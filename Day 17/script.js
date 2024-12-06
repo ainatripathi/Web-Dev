49e0936af59816e1667198d3346d78100c03a735
Aina Tripathi <ainatripathi07@gmail.com>
2024-12-06 11:13:09 +0530

    Add files via upload

---
+++ Day 17/script.js
@@ -0,0 +1,16 @@
+function greet(name){
+    return `Hello ${name}!`;
+}
+console.log(greet("Developer"));
+const multiply = function(a,b,c)
+{
+    return (a*b*c);
+}
+console.log(multiply(2,3,4));
+//Arrow function
+const add = (x,y) => x+y;
+console.log(add(3,8));
+function sayHi(){
+    console.log("Hi there!");
+}
+sayHi();
