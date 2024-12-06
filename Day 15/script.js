257f85fafce4d183e1b80c9f26584e1533aba976
Aina Tripathi <ainatripathi07@gmail.com>
2024-12-06 10:46:13 +0530

    script.js

--- Day 15/script.js
+++ Day 15/script.js
@@ -8,8 +8,8 @@ console.log(emptyArray);
 let arr=[1,2,3];
 arr.push(4);
 console.log(arr);
-// arr.pop();
-// console.log(arr);
-// arr.shift();
-// console.log(arr);
+arr.pop();
+console.log(arr);
+arr.shift();
+console.log(arr);
 
